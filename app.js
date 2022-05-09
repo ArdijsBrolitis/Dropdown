b643d7bce6844cc43fa6e01e76417efc2db6b8ad
ArdijsBrolitis <archijsb@gmail.com>
2022-05-09 21:58:00 +0300

    Ievietoju līdz šim pievienoto kodu

---
+++ app.js
@@ -0,0 +1,16 @@
+let h = document.getElementById('text');
+
+
+
+const color = ['red', 'green', 'blue']; 
+
+
+for (let i = 0; i < color.length; i++) {
+}
+
+
+
+
+
+ const Valoda = ['Latviešu', 'English', 'Russian'];
+    select = document.getElementById('valstis');
